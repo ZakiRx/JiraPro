c844dcdad7ff67709be1fab92945e3dc06c9d00c
zguemi <zakariaguemi18@gmail.com>
2025-10-24 17:56:41 +0100

    config:init project

---
+++ src/main/java/com/zgdev/jirapro/JiraProApplication.java
@@ -0,0 +1,13 @@
+package com.zgdev.jirapro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiraProApplication.class, args);
+    }
+
+}
