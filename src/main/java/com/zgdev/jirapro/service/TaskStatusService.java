2a8ebd21e2f33c48f181d9b683dd055070cc7c8f
zguemi <zakariaguemi18@gmail.com>
2025-10-26 13:32:01 +0100

    fix: fix test

--- src/main/java/com/zgdev/jirapro/service/TaskStatusService.java
+++ src/main/java/com/zgdev/jirapro/service/TaskStatusService.java
@@ -9,6 +9,6 @@ public interface TaskStatusService {
     Optional<List<TaskStatus>> getTaskStatus();
     Optional<TaskStatus> getTaskStatusById(long id);
     Optional<TaskStatus> createTaskStatus(TaskStatus TaskStatus);
-    Optional<TaskStatus> removeTaskStatus(TaskStatus TaskStatus);
+    void removeTaskStatus(TaskStatus TaskStatus);
     Optional<TaskStatus>  getTaskStatusByStatus(String status);
 }
